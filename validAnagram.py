c5572788fa8d912cea6511ffb547a55c60c0cdeb
Surya-2107 <suryabe09@gmail.com>
2025-12-05 20:16:04 +0530

    Add files via upload

---
+++ validAnagram.py
@@ -0,0 +1,19 @@
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+
+    for char in t:
+        if char not in count or count[char] == 0:
+            return False
+        count[char] = count[char] - 1
+
+    return True
+
+s = "racecar"
+t = "carcare"
+print(isAnagram(s, t))
